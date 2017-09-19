12fad74441c2b1ca198463dd14c9bdb3c98ae5b1
Isaranu Janthong <isaranujanthong@yahoo.com>
2017-09-19 20:35:30 +0700

    Create hello_world_m5stack.ino

---
+++ hello_world_m5stack.ino
@@ -0,0 +1,16 @@
+#include <M5Stack.h>
+ 
+// the setup routine runs once when M5Stack starts up
+void setup(){
+
+  // Initialize the M5Stack object
+  m5.begin();
+  
+  // LCD display
+  m5.Lcd.printf("hello world");
+}
+
+// the loop routine runs over and over again forever
+void loop() {
+
+}
